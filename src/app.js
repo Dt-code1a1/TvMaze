07bbafa2201f8c662b154309415badfc9ccf2bb6
Dt-code1a1 <danielt1113@gmail.com>
2022-05-16 14:11:34 +0200

    Added possibility to change displayed shows and added validation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import {mapListToDOMElements} from './dominteractions.js'
+import {mapListToDOMElements, createDOMElem} from './dominteractions.js'
+import {getShowsByKey} from './requests.js'
 
 class TvMaze{
     constructor(){
@@ -6,10 +7,12 @@ class TvMaze{
         this.showNameButtons = {}
         this.selectedName = "harry"
         this.initializeApp()
+
     }
     initializeApp = () =>{
         this.connectDOMElements()
-        this.setupListeners
+        this.setupListeners()
+
     }
     connectDOMElements = () =>{
         const listOfIds = Array.from(document.querySelectorAll("[id]")).map(elem => elem.id)
@@ -17,17 +20,53 @@ class TvMaze{
 
         this.viewElems = mapListToDOMElements(listOfIds, "id")
         this.showNameButtons = mapListToDOMElements(listOfShowNames, "data-show-name")
-        console.log(this.showNameButtons)
     }
+
     setupListeners = () =>{
         Object.keys(this.showNameButtons).forEach(showName => {
             this.showNameButtons[showName].addEventListener('click', this.setCurrentNameFilter)
-            console.log(this.selectedName)
         })
     }
+
     setCurrentNameFilter = () =>{
         this.selectedName = event.target.dataset.showName
-        console.log(this.selectedName)
+        this.fetchAndDisplayShows()
+    }
+    fetchAndDisplayShows = () =>{
+        getShowsByKey(this.selectedName).then(shows => this.renderCard(shows))
+    }
+    renderCard = shows =>{
+        this.viewElems.showsWrapper.innerHTML = ""
+        for(const { show } of shows){
+            this.createShowCard(show)
+        }
+    }
+    createShowCard = show => {
+        const divCard = createDOMElem('div', 'card', null, null)
+        const divCardBody = createDOMElem('div', 'card-body')
+        const h5 = createDOMElem('h5', 'card-title', show.name)
+        const btn = createDOMElem('button', 'btn btn-primary', 'Show details')
+        let img,p
+
+        if(show.image){
+            img = createDOMElem('img', 'card-img-top', null, show.image.medium)
+        }else{
+            img = createDOMElem('img', 'card-img-top', null, 'https://via.placeholder.com/210x295')
+        }
+
+        if(show.summary){
+            p = createDOMElem('p', 'card-text', `${show.summary.slice(0,80)}...`)
+        }else{
+            p = createDOMElem('p', 'card-text', 'There is no summary for that show yet.')
+        }
+
+
+        divCard.appendChild(divCardBody)
+        divCardBody.appendChild(img)
+        divCardBody.appendChild(h5)
+        divCardBody.appendChild(p)
+        divCardBody.appendChild(btn)
+        this.viewElems.showsWrapper.appendChild(divCard)
     }
 
 }
--- src/requests.js
+++ src/requests.js
@@ -1,8 +1,8 @@
 export const getShowsByKey = key => {
     return fetch(`https://api.tvmaze.com/search/shows?q=${key}`)
-    .then(resp => resp.json)
+    .then(resp => resp.json())
 }
-export const getShowById = key => {
+export const getShowById = id => {
     return fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`)
-    .then(resp => resp.json)
+    .then(resp => resp.json())
 }
