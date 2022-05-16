3329fc742c5c8dd6609353f2ecd58de575d96c7e
Dt-code1a1 <danielt1113@gmail.com>
2022-05-16 13:01:02 +0200

    Added dom actions and steup listeners

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import {mapListToDOMElements} from './dominteractions.js'
+
+class TvMaze{
+    constructor(){
+        this.viewElems = {}
+        this.showNameButtons = {}
+        this.selectedName = "harry"
+        this.initializeApp()
+    }
+    initializeApp = () =>{
+        this.connectDOMElements()
+        this.setupListeners
+    }
+    connectDOMElements = () =>{
+        const listOfIds = Array.from(document.querySelectorAll("[id]")).map(elem => elem.id)
+        const listOfShowNames = Array.from(document.querySelectorAll("[data-show-name]")).map(elem => elem.dataset.showName)
+
+        this.viewElems = mapListToDOMElements(listOfIds, "id")
+        this.showNameButtons = mapListToDOMElements(listOfShowNames, "data-show-name")
+        console.log(this.showNameButtons)
+    }
+    setupListeners = () =>{
+        Object.keys(this.showNameButtons).forEach(showName => {
+            this.showNameButtons[showName].addEventListener('click', this.setCurrentNameFilter)
+            console.log(this.selectedName)
+        })
+    }
+    setCurrentNameFilter = () =>{
+        this.selectedName = event.target.dataset.showName
+        console.log(this.selectedName)
+    }
+
+}
+document.addEventListener('DOMContentLoaded', new TvMaze())
